67f6964c1f5a92e8f428c9f1db5cbe793fa61303
Michal Kowalik <klapaucius.mk@gmail.com>
2025-09-23 23:23:02 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Email Blocklist
+
+Keep your WordPress site clean by blocking unwanted signups and comments with a blocklist of spam and temporary email domains.
---
+++ email-blocklist.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Plugin Name:       Email Blocklist
+ * Plugin URI:        https://wordpress.org/plugins/email-blocklist/
+ * Description:       Keep your WordPress site clean by blocking unwanted signups and comments with a blocklist of spam and temporary email domains.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Michał Kowalik
+ * Author URI:        https://michalkowalik.pl
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       email-blocklist
+ * Domain Path:       /languages
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
