1759c00326798115173b822776c0efed9913b9b0
Michal Kowalik <klapaucius.mk@gmail.com>
2025-09-26 00:07:21 +0200

    feat: add composer and the helper class

---
+++ App/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Helper
+{
+    public static function logError(string $errorContent): void
+    {
+        error_log(__('Error from the Email Blocklist plugin:', 'email-blocklist') . ' ' . $errorContent);
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'klapaucius4/email-blocklist',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0b42dbe0ab598db41d921245de8b745f53b3b8c1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'klapaucius4/email-blocklist' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0b42dbe0ab598db41d921245de8b745f53b3b8c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
