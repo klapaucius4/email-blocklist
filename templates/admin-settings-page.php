b635d120adf1fd9e4af7362ceea29a5914a9efbc
Michal Kowalik <klapaucius.mk@gmail.com>
2025-09-28 00:05:57 +0200

    feat: add blocklist and allowlist fields to settings page form

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -14,6 +14,31 @@ if (! defined('ABSPATH')) {
 
         <?php do_settings_sections('email-blocklist-settings'); ?>
 
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">
+                    <label for="eb_local_allowlist">
+                        <?php _e('Local allowlist', 'block-temporary-email') ?> (<?php echo count(array_filter(explode("\n", get_option('eb_local_allowlist')))); ?>)
+                    </label>
+                </th>
+                <td>
+                    <textarea rows="3" class="regular-text" id="eb_local_allowlist" name="eb_local_allowlist"><?php echo esc_attr(get_option('eb_local_allowlist')); ?></textarea>
+                    <p class="description"><?php _e('One domain name per line.', 'block-temporary-email') ?></p>
+                </td>
+            </tr>
+            <tr style="vertical-align: top">
+                <th scope="row">
+                    <label for="eb_local_blocklist">
+                        <?php _e('Local blocklist', 'block-temporary-email') ?> (<?php echo count(array_filter(explode("\n", get_option('eb_local_blocklist')))); ?>)
+                    </label>
+                </th>
+                <td>
+                    <textarea rows="3" class="regular-text" id="eb_local_blocklist" name="eb_local_blocklist"><?php echo esc_attr(get_option('eb_local_blocklist')); ?></textarea>
+                    <p class="description"><?php _e('One domain name per line.', 'block-temporary-email') ?></p>
+                </td>
+            </tr>
+        </table>
+
         <?php submit_button(); ?>
     </form>
 </div>
