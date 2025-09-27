ac56cc4da3bfbd334f7985aef7c75a16c3107fc8
Michal Kowalik <klapaucius.mk@gmail.com>
2025-09-27 23:41:30 +0200

    feat: add basic content of admin settings page

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Lorem ipsum dolor...";
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+<div class="wrap">
+    <h2><?php echo __('Email Blocklist Settings', 'email-blocklist'); ?></h2>
+
+    <form method="post" action="options.php">
+        <?php settings_fields('email-blocklist-settings-group'); ?>
+
+        <?php do_settings_sections('email-blocklist-settings'); ?>
+
+        <?php submit_button(); ?>
+    </form>
+</div>
