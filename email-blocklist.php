a1c3f0d28b31b46f553c9f8ef9794be891b77b72
Michal Kowalik <klapaucius.mk@gmail.com>
2025-11-03 22:51:27 +0100

    docs: change the required PHP version

--- email-blocklist.php
+++ email-blocklist.php
@@ -6,7 +6,7 @@
  * Description:       Keep your WordPress site clean by blocking unwanted signups and comments with a blocklist of spam and temporary email domains.
  * Version:           1.0.0
  * Requires at least: 5.2
- * Requires PHP:      7.4
+ * Requires PHP:      8.0
  * Author:            Michał Kowalik
  * Author URI:        https://michalkowalik.pl
  * License:           GPL v2 or later
