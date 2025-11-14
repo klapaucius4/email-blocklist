756afd517df8a21d6e18aaa91265163a9844a0e6
Michal Kowalik <klapaucius.mk@gmail.com>
2025-11-14 20:56:00 +0100

    refactor: change esc_attr to esc_url method for url

--- templates/admin-settings-page.php
+++ templates/admin-settings-page.php
@@ -71,7 +71,7 @@ use EmailBlocklist\Helper;
                     <p class="label-desc"><?php _e('Number of domains', 'email-blocklist'); ?>: <?php echo Helper::getGlobalBlocklistCount(); ?></p>
                     <p class="label-desc"><?php _e('Blocklist version', 'email-blocklist'); ?>: <?php echo get_option('eb_global_blocklist_version', '-'); ?></p>
                     <p class="label-desc"><?php _e('Updated', 'email-blocklist'); ?>: <?php echo date('Y-m-d H:i:s', get_option('eb_global_blocklist_update_timestamp', 0)) ?></p>
-                    <p class="mb-0"><a href="<?php echo esc_attr(Helper::getUpdateGlobalBlocklistUrl()); ?>" class="button"><?php _e('Update global blocklist', 'email-blocklist'); ?></a></p>
+                    <p class="mb-0"><a href="<?php echo esc_url(Helper::getUpdateGlobalBlocklistUrl()); ?>" class="button"><?php _e('Update global blocklist', 'email-blocklist'); ?></a></p>
                     <p class="label-desc"><?php _e('(the global blocklist is automatically updated daily by WP-Cron)', 'email-blocklist'); ?></p>
                 </th>
                 <td>
