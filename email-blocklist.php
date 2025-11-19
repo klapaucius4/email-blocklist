e95177d068be5551a95a41b672eb40e2d1cc4066
Michal Kowalik <klapaucius.mk@gmail.com>
2025-11-19 22:54:48 +0100

    chore: remove the domain path from plugin header

--- email-blocklist.php
+++ email-blocklist.php
@@ -12,7 +12,6 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       email-blocklist
- * Domain Path:       /languages
  */
 
 if (! defined('ABSPATH')) {
